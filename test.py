dc9031932e02e2ff921e36949e56f8e7a9da5ce2
redsarvesh <saursarv@gmail.com>
2017-12-15 00:19:34 +0530

    testing

---
+++ test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python
+
+class fun:
+	def __init__(self):
+		print 'testing'
+	def funny(self):
+		print 'sir' 
+f=fun()
+f.funny()
+
---
+++ test2.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python
+
+class fun():
+	def fun(a):
+		print 'testing'
+
+f=fun()
+f.fun()
